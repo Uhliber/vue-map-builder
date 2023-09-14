cccdff1c44821ad340419466f49e3ccc05cd149b
Oliver Cape <olvrcp@gmail.com>
2023-09-14 18:29:53 +0800

    update README.md

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-map-maker/',
+  base: '/vue-map-builder/',
   plugins: [vue()],
   resolve: {
     alias: [
